e109f4ed5b828cedeec12b154f66bac57d357bb4
= <=>
2026-04-07 17:13:56 +0200

    chore: Testar publicering

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
-  base: '/autodep/',
+  base: 'autodep',
   plugins: [
     ViteImageOptimizer({
       jpeg: { quality: 90},
