ec19796ea94989f5871e62de36f102e2410b6d64
= <=>
2026-04-07 11:49:36 +0200

    chore: Korrigera sökvägar för rutterna i navigeringsfältet

--- src/logic/data/routes.js
+++ src/logic/data/routes.js
@@ -1,7 +1,7 @@
 import { Route } from "../model/Route.js";
 
 export const ROUTES = [
-  new Route('Laboration', './lab.html'),
+  new Route('Laboration', './index.html'),
   new Route('Bilder', './gallery.html'),
   new Route('Arbetsprocess', './workflow.html')
 ];
