6621e80367440e22503f36dfb9165e7af476a656
= <=>
2026-04-07 17:02:53 +0200

    chore: Testa publicering

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
+  base: '/autodep/',
   plugins: [
     ViteImageOptimizer({
       jpeg: { quality: 90},
