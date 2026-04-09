142c978fc164050b13120a0cdeebe87c7ed847b5
= <=>
2026-04-09 12:42:29 +0200

    chore: Lägg till informationen om arbetsprocessen

--- src/ui/main.js
+++
@@ -1,5 +0,0 @@
-import { ROUTES } from "../logic/data/routes.js";
-import { Nav } from "./units/Nav.js";
-import { node } from "./utils/utils.js";
-
-new Nav(node('navbar'), ROUTES); 
---
+++ src/ui/views/nav-view.js
@@ -0,0 +1,5 @@
+import { ROUTES } from "../../logic/data/routes.js";
+import { Nav } from "../units/Nav.js";
+import { node } from "../utils/utils.js";
+
+new Nav(node('navbar'), ROUTES); 
